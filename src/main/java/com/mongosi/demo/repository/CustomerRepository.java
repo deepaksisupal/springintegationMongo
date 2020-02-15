11251af8012f7587b872781782a77dc6247b41a6
deepaksisupal <deepaksisupal@gmail.coml>
2020-02-15 14:23:44 +0000

    all

--- src/main/java/com/mongosi/demo/repository/CustomerRepository.java
+++ src/main/java/com/mongosi/demo/repository/CustomerRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ProductRepository extends MongoRepository<Customer, String> {
+public interface CustomerRepository extends MongoRepository<Customer, String> {
 
 }
